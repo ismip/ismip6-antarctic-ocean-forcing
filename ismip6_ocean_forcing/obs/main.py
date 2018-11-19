37dbaaf896a04e5051790f2020d5c5e065ac8b2e
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-11-18 19:38:51 -0700

    Process EN4 data
    Since EN4 supplies potential temperature, convert to in situ temp,
    sharing code with the model module.
    
    Combine WOA, MEOP and EN4 into a single data set.

--- ismip6_ocean_forcing/obs/main.py
+++ ismip6_ocean_forcing/obs/main.py
@@ -1,7 +1,11 @@
 import os
+import xarray
+import numpy
+
 from ismip6_ocean_forcing.obs import extrap
 from ismip6_ocean_forcing.woa.main import process_woa
 from ismip6_ocean_forcing.meop.main import process_meop
+from ismip6_ocean_forcing.en4.main import process_en4
 from ismip6_ocean_forcing.thermal_forcing.main import compute_thermal_forcing
 from ismip6_ocean_forcing.remap.res import get_res
 
@@ -22,10 +26,13 @@ def process_obs(config):
 
     resFinal = get_res(config, extrap=False)
 
-    decades = '1995-2017'
+    startYear = 1995
+    endYear = 2017
+    decades = '{:04d}-{:04d}'.format(startYear, endYear)
 
     process_woa(config, decades)
     process_meop(config)
+    process_en4(config, startYear, endYear)
 
     print('Combining and extrapolating observations...')
 
@@ -46,5 +53,37 @@ def process_obs(config):
 
 def _combine_obs(config, decades):
     print('  Combining observations...')
+    res = get_res(config, extrap=True)
+    for fieldName in ['temperature', 'salinity']:
+        outFileName = 'obs/obs_{}_{}_{}.nc'.format(fieldName, decades, res)
+        if os.path.exists(outFileName):
+            continue
+        datasets = {}
+        datasets['woa'] = xarray.open_dataset('woa/woa_{}_{}_{}.nc'.format(
+                    fieldName, decades, res))
+        datasets['meop'] = xarray.open_dataset('meop/meop_{}_{}.nc'.format(
+                fieldName, res))
+        datasets['en4'] = xarray.open_dataset('en4/en4_{}_{}_{}.nc'.format(
+                    fieldName, decades, res))
+        ds = datasets['en4'].drop(fieldName)
+        nx = ds.sizes['x']
+        ny = ds.sizes['y']
+        nz = ds.sizes['z']
+        field = numpy.zeros((nz, ny, nx))
+        weights = numpy.zeros(field.shape)
+        for name in datasets:
+            localField = datasets[name][fieldName].values
+            mask = numpy.isfinite(localField)
+            field[mask] += localField[mask]
+            weights[mask] += 1.
+
+        mask = weights > 0
+        field[mask] /= weights[mask]
+        field[numpy.logical_not(mask)] = numpy.nan
+        ds[fieldName] = (('z', 'y', 'x'), field)
+        ds[fieldName].attrs = datasets['en4'][fieldName].attrs
+
+        ds.to_netcdf(outFileName)
+
     print('    Done.')
 
--- ismip6_ocean_forcing/thermal_forcing/main.py
+++ ismip6_ocean_forcing/thermal_forcing/main.py
@@ -78,3 +78,47 @@ def compute_thermal_forcing(temperatureFileName, salinityFileName, outFileName,
         "between the temperature and the local freezing point"
 
     ds.to_netcdf(outFileName)
+
+
+def potential_to_in_situ_temperature(dsPotTemp, dsSalin):
+    z = dsPotTemp.z.values
+    lat = numpy.maximum(dsPotTemp.lat.values, -80.)
+    lon = dsPotTemp.lon.values
+
+    nz = len(z)
+    ny, nx = lat.shape
+
+    if 'time' in dsPotTemp.dims:
+        nt = dsPotTemp.sizes['time']
+        T = numpy.nan*numpy.ones((nt, nz, ny, nx))
+        for zIndex in range(nz):
+            pressure = gsw.p_from_z(z[zIndex], lat)
+            for tIndex in range(nt):
+                pt = dsPotTemp.temperature[tIndex, zIndex, :, :].values
+                salin = dsSalin.salinity[tIndex, zIndex, :, :].values
+                mask = numpy.logical_and(numpy.isfinite(pt), numpy.isfinite(salin))
+                SA = gsw.SA_from_SP(salin[mask], pressure[mask], lon[mask],
+                                    lat[mask])
+                TSlice = T[tIndex, zIndex, :, :]
+                CT = gsw.CT_from_pt(SA, pt[mask])
+                TSlice[mask] = gsw.t_from_CT(SA, CT, pressure[mask])
+                T[tIndex, zIndex, :, :] = TSlice
+    else:
+        T = numpy.nan*numpy.ones((nz, ny, nx))
+        for zIndex in range(nz):
+            pressure = gsw.p_from_z(z[zIndex], lat)
+            pt = dsPotTemp.temperature[zIndex, :, :].values
+            salin = dsSalin.salinity[zIndex, :, :].values
+            mask = numpy.logical_and(numpy.isfinite(pt), numpy.isfinite(salin))
+            SA = gsw.SA_from_SP(salin[mask], pressure[mask], lon[mask],
+                                lat[mask])
+            TSlice = T[zIndex, :, :]
+            CT = gsw.CT_from_pt(SA, pt[mask])
+            TSlice[mask] = gsw.t_from_CT(SA, CT, pressure[mask])
+            T[zIndex, :, :] = TSlice
+
+    dsTemp = dsPotTemp.drop('temperature')
+    dsTemp['temperature'] = (dsPotTemp.temperature.dims, T)
+    dsTemp['temperature'].attrs = dsPotTemp.temperature.attrs
+
+    return dsTemp
