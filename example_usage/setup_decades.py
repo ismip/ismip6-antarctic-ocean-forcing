d2a4d8719333de6c8be3d4abc0516a6c166dc1c3
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-12-18 14:39:34 -0700

    Add example usage with templates

---
+++ example_usage/setup_decades.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import os
+
+def replace(inFileName, outFileName, replacements):
+    with open(inFileName, 'rt') as fin:
+        with open(outFileName, 'wt') as fout:
+            for line in fin:
+                for orig in replacements:
+                    line = line.replace(orig, replacements[orig])
+                fout.write(line)
+
+climFirstYear = 1995
+climLastYear = 2014
+climOutFolder = '{:04d}-{:04d}'.format(climFirstYear, climLastYear)
+climFolders = ', '.join(['{:04d}'.format(year) for year in 
+                         range(climFirstYear, climLastYear+1)])
+
+firstYears = list(range(1850, 1994, 20)) + list(range(1995, 2100, 20))
+
+for firstYear in firstYears:
+    if firstYear == 1990:
+        lastYear = 1994
+    elif firstYear == 2095:
+        lastYear = 2100
+    else:
+        lastYear = firstYear+19
+
+    outFolder = '{:04d}-{:04d}'.format(firstYear, lastYear)
+    folders = ', '.join(['{:04d}'.format(year) for year in 
+                         range(firstYear, lastYear+1)])
+    print(outFolder)
+    try:
+        os.makedirs(outFolder)
+    except OSError:
+        pass
+
+    replacements = {'@outFolder': outFolder,
+                    '@folders': folders,
+                    '@years': outFolder,
+                    '@tIndexMin': '{}'.format(firstYear-1850),
+                    '@tIndexMax': '{}'.format(lastYear-1850),
+                    '@climDecades': climOutFolder,
+                    '@climFolders': climFolders,
+                    '@climFirstTIndex':'0',
+                    '@climLastTIndex':'{}'.format(climLastYear-climFirstYear)}
+    templateFileName = 'config.combine_template'
+    outFileName = '{}/config.ccsm4'.format(outFolder)
+    replace(templateFileName, outFileName, replacements)
+    templateFileName = 'job_script_combine_template.bash'
+    outFileName = '{}/job_script.bash'.format(outFolder)
+    replace(templateFileName, outFileName, replacements)
---
+++ example_usage/setup_years.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+
+def replace(inFileName, outFileName, replacements):
+    with open(inFileName, 'rt') as fin:
+        with open(outFileName, 'wt') as fout:
+            for line in fin:
+                for orig in replacements:
+                    line = line.replace(orig, replacements[orig])
+                fout.write(line)
+
+
+for tIndex, year in enumerate(range(1850, 2101)):
+    yearString = '{:04d}'.format(year)
+    print(yearString)
+    try:
+        os.makedirs(yearString)
+    except OSError:
+        pass
+
+    replacements = {'@tIndex': '{}'.format(tIndex),
+                    '@year': yearString}
+    templateFileName = 'config.template'
+    outFileName = '{}/config.ccsm4'.format(yearString)
+    replace(templateFileName, outFileName, replacements)
+    templateFileName = 'job_script_template.bash'
+    outFileName = '{}/job_script.bash'.format(yearString)
+    replace(templateFileName, outFileName, replacements)
