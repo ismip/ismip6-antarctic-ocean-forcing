e88ffab0b452e824f0d0ccad2870b7ef8880a455
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-03-29 09:49:50 +0100

    Fix code for newer descartes

--- ismip6_ocean_forcing/imbie/images.py
+++ ismip6_ocean_forcing/imbie/images.py
@@ -95,10 +95,12 @@ def _write_basin_image(res, basinShape, name, nx, ny, dx):
     color = 'black'
 
     if basinShape.geom_type == 'Polygon':
-        ax.add_patch(PolygonPatch(basinShape, fc=color, ec=color, linewidth=0))
+        ax.add_patch(PolygonPatch(
+            basinShape.__geo_interface__, fc=color, ec=color, linewidth=0))
     elif basinShape.geom_type == 'MultiPolygon':
         for poly in basinShape:
-            ax.add_patch(PolygonPatch(poly, fc=color, ec=color, linewidth=0))
+            ax.add_patch(PolygonPatch(
+                poly.__geo_interface__, fc=color, ec=color, linewidth=0))
 
     plt.xlim([-dx*0.5*(nx+1), dx*0.5*(nx+1)])
     plt.ylim([-dx*0.5*(ny+1), dx*0.5*(ny+1)])
